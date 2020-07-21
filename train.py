0b0332e775031d01f12e81eb143f67e5a703f300
zentornoe <58382336+zentornoe@users.noreply.github.com>
2020-07-21 20:11:28 +0900

    Update train.py

--- train.py
+++ train.py
@@ -1,24 +1,33 @@
-import glob
 import numpy as np
 import matplotlib.pyplot as plt
 import os
-from keras.layers import Input, Activation, Conv2D, Flatten, Dense, MaxPooling2D, Dropout, Add, LeakyReLU, UpSampling2D
+import tensorflow as tf
+from keras.layers import Input, Activation, Conv2D, Flatten, Dense, MaxPooling2D, Dropout, Add, LeakyReLU, UpSampling2D, \
+    Conv2DTranspose
 from keras.models import Model, load_model
 from keras.callbacks import ReduceLROnPlateau
-os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
+
+
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
 base_dir = './'
 img_dir = './images/'
 
-x_train = np.load('./dataset/x_data.npy')
-y_train = np.load('./dataset/y_data.npy')
-x_val = x_train[40:-1]
-y_val = y_train[40:-1]
-x_train = x_train[:40]
-y_train = y_train[:40]
+checkpoint_path = './checkpoint/cp-{epoch:04d}.ckpt'
+checkpoint_dir = os.path.dirname(checkpoint_path)
+cp_callback = tf.keras.callbacks.ModelCheckpoint(checkpoint_path, save_weights_only=True, verbose=1,
+                                                 period=100)
 
-print(x_train.shape, y_train.shape)
 
-inputs = Input(shape=(256, 256, 1))
+x_train = np.load('dataset/bone/x_data.npy')
+y_train_onehot = np.load('dataset/bone/y_data_onehot.npy')
+y_val = np.load('dataset/bone/y_data.npy')
+x_val = x_train[:20]
+y_val_onehot = y_train_onehot[:20]
+x_train = x_train[20:]
+y_train_onehot = y_train_onehot[20:]
+print(x_train.shape, y_train_onehot.shape)
+
+inputs = Input(shape=(640, 640, 1))
 
 net = Conv2D(32, kernel_size=3, activation='relu', padding='same')(inputs)
 net = MaxPooling2D(pool_size=2, padding='same')(net)
@@ -29,27 +38,29 @@ net = MaxPooling2D(pool_size=2, padding='same')(net)
 net = Conv2D(128, kernel_size=3, activation='relu', padding='same')(net)
 net = MaxPooling2D(pool_size=2, padding='same')(net)
 
-net = Dense(128, activation='relu')(net)
+net = Conv2D(128, kernel_size=1, activation='relu')(net)
 
-net = UpSampling2D(size=2)(net)
+#net = UpSampling2D(size=2)(net)
+net = Conv2DTranspose(128, kernel_size=1, strides=2, padding='same', activation='relu')(net)
 net = Conv2D(128, kernel_size=3, activation='sigmoid', padding='same')(net)
 
-net = UpSampling2D(size=2)(net)
+#net = UpSampling2D(size=2)(net)
+net = Conv2DTranspose(128, kernel_size=1, strides=2, padding='same', activation='relu')(net)
 net = Conv2D(64, kernel_size=3, activation='sigmoid', padding='same')(net)
 
-net = UpSampling2D(size=2)(net)
-outputs = Conv2D(1, kernel_size=3, activation='sigmoid', padding='same')(net)
+#net = UpSampling2D(size=2)(net)
+net = Conv2DTranspose(64, kernel_size=1, strides=2, padding='same', activation='relu')(net)
+outputs = Conv2D(2, kernel_size=3, activation='sigmoid', padding='same')(net)
 
 model = Model(inputs=inputs, outputs=outputs)
 
-model.compile(optimizer='adam', loss='binary_crossentropy',
-              metrics=['acc', 'mse'])
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc', 'mse'])
 
 model.summary()
 
-history = model.fit(x_train, y_train, validation_data=(x_val, y_val), epochs=100, batch_size=20, callbacks=[
+history = model.fit(x_train, y_train_onehot, validation_data=(x_val, y_val_onehot), epochs=5000, batch_size=10, callbacks=[
     ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=10, verbose=1,
-                      mode='auto', min_lr=1e-05)
+                      mode='auto', min_lr=1e-05), cp_callback
 ])
 
 fig, ax = plt.subplots(2, 2, figsize=(10, 7))
@@ -65,7 +76,8 @@ ax[1, 1].set_title('val_acc')
 ax[1, 1].plot(history.history['val_acc'], 'b--')
 
 preds = model.predict(x_val)
-
+preds = preds[:7]
+preds = np.argmax(preds, axis=-1)
 fig, ax = plt.subplots(len(x_val), 3, figsize=(10, 100))
 plt.figure(figsize=(14, 6))
 for i, pred in enumerate(preds):
@@ -75,3 +87,5 @@ for i, pred in enumerate(preds):
     plt.imshow(y_val[i].squeeze(), cmap='gray')
     plt.subplot(7, 3, 3*i+3)
     plt.imshow(pred.squeeze(), cmap='gray')
+
+plt.show()
